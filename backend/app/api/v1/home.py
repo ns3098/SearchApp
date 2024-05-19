40405d61a02aef1e369bd91d18a96973361cfb6b
ns3098 <nitinsingh1759@gmail.com>
2024-05-19 16:50:55 +0530

    ignore case while querying

--- backend/app/api/v1/home.py
+++ backend/app/api/v1/home.py
@@ -13,7 +13,7 @@ async def home(assembly: str, text: str, page: Optional[int] = 1, page_size: Opt
     assembly_collection = ASSEMBLY_TO_COLLECTION_MAP.get(assembly, "")
     if not assembly_collection:
         raise HTTPException(status_code=400, detail=f"Bad Request: Invalid assembly name {assembly}") 
-    query = Q(voter_id__startswith=text)
+    query = Q(voter_id__istartswith=text)
     skip_value = (page - 1) * page_size
     voter_info = ASSEMBLY_TO_COLLECTION_FACTORY.get(assembly_collection).objects(query).exclude("id").skip(skip_value).limit(page_size).as_pymongo().batch_size(10000).values_list(*FIELDS)
     return JSONResponse(content={"data": list(voter_info)}, headers=HEADERS)
@@ -25,7 +25,7 @@ async def home(assembly: str, text: str, page: Optional[int] = 1, page_size: Opt
     assembly_collection = ASSEMBLY_TO_COLLECTION_MAP.get(assembly, "")
     if not assembly_collection:
         raise HTTPException(status_code=400, detail=f"Bad Request: Invalid assembly name {assembly}") 
-    query = Q(voter_name_eng__startswith=text)
+    query = Q(voter_name_eng__istartswith=text)
     skip_value = (page - 1) * page_size
     voter_info = ASSEMBLY_TO_COLLECTION_FACTORY.get(assembly_collection).objects(query).exclude("id").skip(skip_value).limit(page_size).as_pymongo().batch_size(10000).values_list(*FIELDS)
     return JSONResponse(content={"data": list(voter_info)}, headers=HEADERS)
@@ -36,7 +36,7 @@ async def home(assembly: str, text: str, page: Optional[int] = 1, page_size: Opt
     assembly_collection = ASSEMBLY_TO_COLLECTION_MAP.get(assembly, "")
     if not assembly_collection:
         raise HTTPException(status_code=400, detail=f"Bad Request: Invalid assembly name {assembly}") 
-    query = Q(voter_id__startswith=text)
+    query = Q(voter_id__istartswith=text)
     skip_value = (page - 1) * page_size
     voter_info = ASSEMBLY_TO_COLLECTION_FACTORY.get(assembly_collection).objects(query).skip(skip_value).limit(page_size).as_pymongo().batch_size(10000).values_list('voter_id')
     voters_info = [x['Voter ID'] for x in voter_info]
@@ -48,7 +48,7 @@ async def home(assembly: str, text: str, page: Optional[int] = 1, page_size: Opt
     assembly_collection = ASSEMBLY_TO_COLLECTION_MAP.get(assembly, "")
     if not assembly_collection:
         raise HTTPException(status_code=400, detail=f"Bad Request: Invalid assembly name {assembly}") 
-    query = Q(voter_name_eng__startswith=text)
+    query = Q(voter_name_eng__istartswith=text)
     skip_value = (page - 1) * page_size
     voter_info = ASSEMBLY_TO_COLLECTION_FACTORY.get(assembly_collection).objects(query).skip(skip_value).limit(page_size).as_pymongo().batch_size(10000).values_list('voter_name_eng')
     voters_info = [x['Voter Name Eng'] for x in voter_info]
@@ -61,7 +61,7 @@ async def home(assembly: str, text: str):
     assembly_collection = ASSEMBLY_TO_COLLECTION_MAP.get(assembly, "")
     if not assembly_collection:
         raise HTTPException(status_code=400, detail=f"Bad Request: Invalid assembly name {assembly}") 
-    query = Q(voter_id__startswith=text)
+    query = Q(voter_id__istartswith=text)
     voter_info = ASSEMBLY_TO_COLLECTION_FACTORY.get(assembly_collection).objects(query).as_pymongo().batch_size(10000)
     count = voter_info.count()
     return JSONResponse(content={"data": count}, headers=HEADERS)
@@ -73,7 +73,7 @@ async def home(assembly: str, text: str):
     assembly_collection = ASSEMBLY_TO_COLLECTION_MAP.get(assembly, "")
     if not assembly_collection:
         raise HTTPException(status_code=400, detail=f"Bad Request: Invalid assembly name {assembly}") 
-    query = Q(voter_name_eng__startswith=text)
+    query = Q(voter_name_eng__istartswith=text)
     voter_info = ASSEMBLY_TO_COLLECTION_FACTORY.get(assembly_collection).objects(query).as_pymongo().batch_size(10000)
     count = voter_info.count()
     return JSONResponse(content={"data": count}, headers=HEADERS)
