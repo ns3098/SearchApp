ac3ec9f47c095ba99bb80ca3244a61dbe097a5ef
ns3098 <nitinsingh1759@gmail.com>
2024-05-19 15:54:11 +0530

    get page count

--- backend/app/api/v1/home.py
+++ backend/app/api/v1/home.py
@@ -55,6 +55,28 @@ async def home(assembly: str, text: str, page: Optional[int] = 1, page_size: Opt
     return JSONResponse(content={"data": voters_info}, headers=HEADERS)
 
 
+@router.get("/fetch_epic_count/")
+async def home(assembly: str, text: str):
+    """Home endpoint."""
+    assembly_collection = ASSEMBLY_TO_COLLECTION_MAP.get(assembly, "")
+    if not assembly_collection:
+        raise HTTPException(status_code=400, detail=f"Bad Request: Invalid assembly name {assembly}") 
+    query = Q(voter_id__startswith=text)
+    voter_info = ASSEMBLY_TO_COLLECTION_FACTORY.get(assembly_collection).objects(query).as_pymongo().batch_size(10000)
+    count = voter_info.count()
+    return JSONResponse(content={"data": count}, headers=HEADERS)
+
+
+@router.get("/fetch_name_count/")
+async def home(assembly: str, text: str):
+    """Home endpoint."""
+    assembly_collection = ASSEMBLY_TO_COLLECTION_MAP.get(assembly, "")
+    if not assembly_collection:
+        raise HTTPException(status_code=400, detail=f"Bad Request: Invalid assembly name {assembly}") 
+    query = Q(voter_name_eng__startswith=text)
+    voter_info = ASSEMBLY_TO_COLLECTION_FACTORY.get(assembly_collection).objects(query).as_pymongo().batch_size(10000)
+    count = voter_info.count()
+    return JSONResponse(content={"data": count}, headers=HEADERS)
 
 @router.get("/get_assembly")
 async def get_all_assembly():
